ea32f5921f180a912edc0086a3f023d55dc2e357
Andy Chen <anche@microsoft.com>
2015-10-16 15:32:00 -0700

    fix a recursive call bug

--- index.js
+++ index.js
@@ -11,14 +11,23 @@ Async.prototype = {
 		if(typeof this.iterator === 'undefined' || this.iterator === null){
 			this.iterator = this.generator(this);
 			this.iterator.done = false;
+            this.iterator.running = false;
 		}
 		
 		if(typeof this.iterator.done !== 'undefined' && this.iterator.done){
 			return this;
 		}
-		
+        
+        if(this.iterator.running){
+            this.prevResults = arguments;
+            process.nextTick(this._run_async.bind(this));
+            return;
+        }
+        
+        this.iterator.running = true;
 		var result = this.iterator.next(arguments);
 		this.iterator.done = result.done;
+        this.iterator.running = false;
 		if(this.iterator.done && (typeof this.continueWith !== 'undefined') && this.continueWith != null){
 			this.continueWith.run();
 		}
@@ -49,7 +58,16 @@ Async.prototype = {
 		
 		task.continueWith = async;
 		return this;
-	}
+	},
+    _run_async:function(){
+        var _args = [];
+		if(typeof this.prevResults !== 'undefined' && typeof this.prevResults.length !== 'undefined'){
+			for(var i = 0; i < this.prevResults.length; ++i){
+				_args.push(this.prevResults[i]);
+			}
+		}
+        this.run.apply(this, _args);
+    }
 };
 
 module.exports = Async;
